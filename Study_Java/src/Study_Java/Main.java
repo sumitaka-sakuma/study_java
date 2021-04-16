1501cea066d87d4e1d1dbd7905d190a79386d001
sakum <sakum@DESKTOP-FN9RSRR>
2021-04-16 17:29:22 +0900

    streamAPI

--- Study_Java/src/Study_Java/Main.java
+++ Study_Java/src/Study_Java/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("hello world");
+		Study_StreamAPI stream = new Study_StreamAPI();
+		stream.streamApiStudy();
 
 	}
 
--- Study_Java/src/Study_Java/Study_StreamAPI.java
+++ Study_Java/src/Study_Java/Study_StreamAPI.java
@@ -1,5 +1,16 @@
 package Study_Java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Study_StreamAPI {
 
+	public void streamApiStudy() {
+
+		// 単純なStream操作
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		Stream<Integer> stream = list.stream();
+		stream.forEach(System.out::println);
+	}
 }
